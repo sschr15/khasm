5798253af81f1fe6938f2949f5369332d0ded88c
sschr15 <steven.sschr15@hotmail.com>
2021-01-17 13:46:06 -0600

    Remove unneeded thing

--- src/main/kotlin/net/khasm/util/KoffeeUtilities.kt
+++ src/main/kotlin/net/khasm/util/KoffeeUtilities.kt
@@ -1,21 +1,13 @@
 package net.khasm.util
 
 import codes.som.anthony.koffee.MethodAssembly
-import codes.som.anthony.koffee.insns.InstructionAssembly
 import codes.som.anthony.koffee.labels.LabelLike
 import codes.som.anthony.koffee.labels.coerceLabel
 import codes.som.anthony.koffee.types.TypeLike
-import org.objectweb.asm.tree.InsnList
 import org.objectweb.asm.tree.LocalVariableNode
-import org.objectweb.asm.tree.MethodNode
 
 fun MethodAssembly.localVar(
     name: String, desc: TypeLike, signature: String?,
     start: LabelLike, end: LabelLike, index: Int
 ) = LocalVariableNode(name, coerceType(desc).descriptor, signature, coerceLabel(start), coerceLabel(end), index)
     .also { node.localVariables.add(it) }
-
-class CrossKoffeeInsnRegistry(private val method: MethodNode) : InstructionAssembly {
-    override val instructions: InsnList
-        get() = method.instructions
-}
