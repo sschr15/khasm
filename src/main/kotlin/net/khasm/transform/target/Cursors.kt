8e984bf793e1be2fb431bed95270251695e58fd8
sschr15 <steven.sschr15@hotmail.com>
2021-01-16 22:53:59 -0600

    this space annoyed me

--- src/main/kotlin/net/khasm/transform/target/Cursors.kt
+++ src/main/kotlin/net/khasm/transform/target/Cursors.kt
@@ -3,6 +3,7 @@ package net.khasm.transform.target
 sealed class Cursors
 
 class CursorRanges(val ranges: List<IntRange>): Cursors()
+
 class CursorsFixed(val points: List<Int>): Cursors() {
     constructor(vararg points: Int) : this(points.toList())
 }
